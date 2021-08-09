1ebc72984d14cbd1b3c4326fada627d4ffe6e42d
ktm <using1524@naver.com>
2021-08-09 12:35:56 +0900

    eureka server 생성(포트 8761)

---
+++ src/test/java/com/exchange/ExchangeEurekaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
